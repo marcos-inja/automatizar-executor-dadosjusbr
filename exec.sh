9c3747b1be940288d5c479b7caf061d9962319db
marcos-vas <marcosvaas@gmail.com>
2021-11-14 15:00:14 -0300

    fix: add month for execution

--- exec.sh
+++ exec.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
-# Declaração das váriaveis:
 # Caminho do executor cmd
 EXECUTOR_CMD=/home/marcos/dadosjusbr/coletar/executor/cmd
+
+# Para facilitar uma execução com os meses com erro: 
+# '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12'
 MONTHS=('01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12')
 
 # Tempo para execução do coletor
