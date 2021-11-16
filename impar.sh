81d73247e359476d83561daac602860acb14cad0
marcos-vas <marcosvaas@gmail.com>
2021-11-16 10:50:15 -0300

    fix: run 2 same time

---
+++ impar.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Caminho do executor cmd
+EXECUTOR_CMD=/home/marcos/dadosjusbr/coletar/executor/cmd
+
+# Para facilitar uma execução com os meses com erro: 
+# '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12'
+MONTHS=('01' '03' '05' '07' '09' '11')
+
+# Tempo para execução do coletor
+DOWNLOAD_TIMEOUT=20s 
+TIME_BETWEEN_STEPS=15s 
+GENERAL_TIMEOUT=8m
+
+# Ano e tribunal lido no inicio:
+echo -e "\033[1;32mQuer rodar para qual ano?\033[0m"
+read YEAR
+echo -e "\033[1;32mPara qual tribunal?\033[0m"
+read COURT
+
+# Entra na pasta do executor, e nela vai ser feito tudo abaixo
+cd $EXECUTOR_CMD
+# Cria uma pasta para o orgão, ela recebe as saida
+mkdir $COURT
+
+for MONTH in ${MONTHS[*]}
+do
+    echo -e "\n\033[40;1;37m Rodando para meses impares, orgão: ${COURT}, ano: ${YEAR} e mês: ${MONTH} \033[0m"
+
+    sudo ./executor --in=${COURT,,}.json \
+    --volume-dir=/output1 --volume-name=dadosjusbr1 \
+    --def-run-env=OUTPUT_FOLDER:/output1 \
+    --def-run-env=MONTH:${MONTH} \
+    --def-run-env=YEAR:${YEAR} \
+    --def-run-env=COURT:${COURT^^} \
+    --def-run-env=DOWNLOAD_TIMEOUT:${DOWNLOAD_TIMEOUT} \
+    --def-run-env=TIME_BETWEEN_STEPS:${TIME_BETWEEN_STEPS} \
+    --def-run-env=GENERAL_TIMEOUT:${GENERAL_TIMEOUT} \
+    > ${COURT,,}/${YEAR}_${MONTH}.out
+    
+    echo -e "\033[42;1;37m Executado com sucesso! \033[0m"
+done
--- par.sh
+++ par.sh
@@ -5,7 +5,7 @@ EXECUTOR_CMD=/home/marcos/dadosjusbr/coletar/executor/cmd
 
 # Para facilitar uma execução com os meses com erro: 
 # '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12'
-MONTHS=('01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12')
+MONTHS=('02''04' '06' '08' '10' '12')
 
 # Tempo para execução do coletor
 DOWNLOAD_TIMEOUT=20s 
@@ -25,16 +25,18 @@ mkdir $COURT
 
 for MONTH in ${MONTHS[*]}
 do
-    echo -e "\n\033[40;1;37m Rodando para o orgão: ${COURT}, ano: ${YEAR} e mês: ${MONTH} \033[0m"
+    echo -e "\n\033[40;1;37m Rodando para meses pares, orgão: ${COURT}, ano: ${YEAR} e mês: ${MONTH} \033[0m"
 
     sudo ./executor --in=${COURT,,}.json \
+    --volume-dir=/output --volume-name=dadosjusbr \
+    --def-run-env=OUTPUT_FOLDER:/output \
     --def-run-env=MONTH:${MONTH} \
     --def-run-env=YEAR:${YEAR} \
     --def-run-env=COURT:${COURT^^} \
     --def-run-env=DOWNLOAD_TIMEOUT:${DOWNLOAD_TIMEOUT} \
     --def-run-env=TIME_BETWEEN_STEPS:${TIME_BETWEEN_STEPS} \
     --def-run-env=GENERAL_TIMEOUT:${GENERAL_TIMEOUT} \
-    > ${COURT,,}/${YEAR}_${MONTH}.out 
-
+    > ${COURT,,}/${YEAR}_${MONTH}.out
+    
     echo -e "\033[42;1;37m Executado com sucesso! \033[0m"
 done
